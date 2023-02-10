3103a92d70d0ea9557c92714971707059ea5c2f5
Salano24 <salernomarco48@email.com>
2023-02-10 12:39:39 +0100

    end jumbo

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <script>
 import AppHeader from './components/AppHeader.vue'
+import AppJumbo from './components/AppJumbo.vue'
 
 export default {
     components: {
         AppHeader,
+        AppJumbo,
+
     }
 }
 </script>
 
 <template>
     <AppHeader />
+    <AppJumbo />
 </template>
 
 
---
+++ src/components/AppJumbo.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+    name: "AppHJumbo",
+}
+</script>
+
+<template>
+<div class="jumbo"></div>
+</template>
+
+<style lang="scss">
+.jumbo{
+    background-image: url(../assets/jumbo.jpg);
+    width: 100vw;
+    aspect-ratio: 16 / 8;
+    
+    background-repeat: no-repeat;
+    background-size: cover;
+}
+</style>
