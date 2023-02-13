ecb3097dfb6cfe126e109a3551f9dd302d691a7b
SimoneLollini <lollinisimone@gmail.com>
2023-02-13 20:26:41 +0100

    add store.getRestaurants

--- src/store.js
+++ src/store.js
@@ -3,7 +3,39 @@ import axios from "axios";
 
 export const store = reactive({
   showMenu: false,
+  restaurants: '',
+  restaurant: '',
+  loading: true,
+  base_api_url: 'http://127.0.0.1:8000/',
   showOffcanvasMenu() {
     this.showMenu = !this.showMenu;
+  },
+  getRestaurants(call) {
+    axios.get(call)
+      .then(response => {
+        this.restaurants = response.data.results;
+        console.log(this.restaurants)
+        this.loading = false
+      })
+      .catch(error => {
+        console.error(error)
+        this.error = error.message
+        this.loading = false
+      })
+  },
+  getRestaurant(call) {
+    axios.get(call)
+      .then(response => {
+        if (response.data.success) {
+          this.restaurant = response.data.results
+          this.loading = false
+        } else {
+          this.$router.replace({ name: 'not-found' })
+        }
+        console.log(response);
+      }).catch(error => {
+        console.log(error)
+      })
+
   },
 });
