5f7889b7c46084be7bf45e304970654e02f46465
Danielateresa <dany.t_87@hotmail.it>
2023-02-13 15:08:14 +0100

    Add side cart

--- src/components/AppCartPreview.vue
+++ src/components/AppCartPreview.vue
@@ -1,117 +1,24 @@
 <script>
+
 export default {
     name: "AppCartPreview",
+    data(){
+    return {
+      showMenu: false
+    }
+  },
+  methods: {
+    showOffcanvasMenu(){
+      this.showMenu ? this.showMenu = false : this.showMenu = true;
+    }
+  }
 }
 </script>
 
 <template>
 
-    <div class="off-canvas">
-
-        <div id="CartDrawer" class="drawer drawer--right open" tabindex="-1">
-            <div class="drawer__header">
-                <span class="drawer__close js-drawer-close dt-sc-btn open close-icon">
-                    x
-                </span>
-                <h4 class="my-3">Il tuo carrello</h4>
-            </div>
-
-
-            <div id="CartContainer">
-
-                <form action="/cart" method="post" novalidate="" class="cart ajaxcart">
-                    <ul class="ajaxcart__inner">
-
-                        <li class="ajaxcart__product">
-                            <div class="ajaxcart row" data-line="1">
-                                <div class="item_img"><a href="/products/toritilla-wrap?variant=39541360099411"
-                                        class="ajaxcart__product-image"> <img
-                                            src="https://cdn.shopify.com/s/files/1/0549/7359/5731/products/shop007_medium.jpg?v=1638158749"
-                                            alt=""></a> </div>
-                                <div class="details_cart">
-
-                                    <h6><a href="/products/toritilla-wrap?variant=39541360099411"
-                                            class="ajaxcart__product-name">Toritilla Wrap</a></h6>
-                                    <div class="item-desc">
-                                        <div class="item_dtl">
-                                            <p class="ajaxcart__product-meta">M / Non Veg</p>
-                                            <div class="pricing">
-                                                <span class="ajaxcart__price">
-                                                    <span class="money">Rs.296.00</span>
-                                                </span>
-                                            </div>
-                                        </div>
-                                        <div class="ajaxcart__qty product-item-caption-qty">
-                                            <button type="button"
-                                                class="ajaxcart__qty-adjust ajaxcart__qty--minus quantity-increment"
-                                                data-id="" data-qty="1" data-line="1">
-                                                <span>−</span>
-                                            </button>
-                                            <input type="text" name="updates[]" class="ajaxcart__qty-num input-number"
-                                                value="2" min="0" data-id="" data-line="1" aria-label="quantity"
-                                                pattern="[0-9]*">
-                                            <button type="button"
-                                                class="ajaxcart__qty-adjust ajaxcart__qty--plus quantity-increment"
-                                                data-id="" data-line="1" data-qty="3">
-                                                <span>+</span>
-                                            </button>
-                                            <span id="changeQty" class="ajaxcart__qty-remove remove-btn  dt-sc-btn"
-                                                data-id="39541360099411:afe9063be9fe031a79240fb6ef1e514b" data-line="1"
-                                                data-qty="3">
-                                                <font-awesome-icon icon="fa-solid fa-trash" />
-                                            </span>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </li>
-                    </ul>
-                    <div class="ajaxcart__footer row">
-                        <div class="subtotal">
-                            <p class="title">Total</p>
-                            <p class="subtotal-price"><span class="cart-original-total money">Rs. 296.00</span></p>
-                        </div>
-
-
-                        <div class="discounts">
-
-
-                        </div>
-                        <div class="total">
-                            <p>Shipping, taxes, and discounts will be calculated at checkout.</p>
-                        </div>
-
-                        <div class="d-flex flex-column px-3">
-                            <router-link :to="{ name: 'checkout' }">
-                                <button type="submit" class="btn btn_orange mb-4 w-100" name="checkout">
-                                    Procedi al Checkout
-                                </button>
-                            </router-link>
-
-
-                            <router-link class="btn btn_orange" :to="{ name: 'cart' }">Vedi il carrello</router-link>
-
-                        </div>
-
-
-
-                    </div>
-                </form>
-
-            </div>
-        </div>
-
-    </div>
-
-
-</template>
+  </template>
 
 <style lang="scss" scoped>
-.off-canvas {
-    top: 0;
-    height: 100vw;
-    width: 100%;
-    position: absolute;
-    z-index: 9999;
-}
+
 </style>
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -3,9 +3,16 @@ import AppCartPreview from './AppCartPreview.vue'
 
 export default {
     name: "AppHeader",
-    components: {
-        AppCartPreview
+    data(){
+    return {
+      showMenu: false
     }
+  },
+  methods: {
+    showOffcanvasMenu(){
+      this.showMenu ? this.showMenu = false : this.showMenu = true;
+    }
+  }
 }
 </script>
 
@@ -37,12 +44,14 @@ export default {
                 <button><i class="fa-solid fa-magnifying-glass"></i></button> -->
 
             </div>
-
+           
             <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse"
                 data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId" aria-expanded="false"
                 aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
+            
+            
             <div class="collapse navbar-collapse" id="collapsibleNavId">
                 <ul class="navbar-nav ms-auto text-end gap-3">
                     <li class="nav-itemhead_user">
@@ -53,14 +62,190 @@ export default {
                        <a href="">Registrati</a>
                     </li>
                     <li class="nav-item cart">
-                        <a href=""><button class="btn btn-primary d-lg-none" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasResponsive" aria-controls="offcanvasResponsive">Cart</button><AppCartPreview /></a>
+                        <button class="cart_icon border-0 bg-white" type="button" @click.prevent="showOffcanvasMenu()"><i class="fa-solid fa-cart-shopping"></i></button>
                     </li>
 
                 </ul>
             </div>
+          
         </div>
-    </nav>
-   
+</nav>
+<!-- cart_preview -->
+    <div v-show="showMenu === true">
+
+        <div class="cart_preview">
+            <div class="cart_right p-4">
+                <div class="cart_preview_header d-flex">
+                    <button type="button" class="btn-close me-4" @click.prevent="showOffcanvasMenu()" aria-label="Close"></button>
+                    <div class="cart_body pb-3">
+                        
+                            <div class="d-flex align-items-center justify-content-between mb-4">
+                                <h4 class="title_frame mb-0">Il tuo ordine</h4>
+                                <div class="square px-2 py-1"><i class="fa-solid fa-utensils text-white"></i></div>
+                            </div>
+                        <div class="cart_content overflow-auto">
+                            <!-- order -->
+                            <a href="#" class="d-flex gap-3 mb-2">
+                                <div class="sb-cover-frame">
+                                    <img src="../assets/plate1.jpg" alt="product">
+                                </div>
+                                <div class="sb-card-tp py-2 pe-2">
+                                    <h4 class="sb-card-title">Insalata</h4>
+                                    <div class="price">6.50 €</div>
+                                </div>
+                            </a>
+                            <div class="quantity_input mb-4">
+                                <div class="plus">-</div>
+                                <input type="number" value="1" min="1" max="10">
+                                <div class="minus">+</div>
+                            </div>
+                            <!-- //order -->
+
+                            <!-- order -->
+                            <a href="#" class="d-flex gap-3 mb-2">
+                                <div class="sb-cover-frame">
+                                    <img src="../assets/hambuerger.jpg" alt="product">
+                                </div>
+                                <div class="sb-card-tp py-2 pe-2">
+                                    <h4 class="sb-card-title">Insalata</h4>
+                                    <div class="price">6.50 €</div>
+                                </div>
+                            </a>
+                            <div class="quantity_input mb-4">
+                                <div class="plus">-</div>
+                                <input type="number" value="1" min="1" max="10">
+                                <div class="minus">+</div>
+                            </div>
+                            <!-- //order -->
+                            
+                            <!-- order -->
+                            <a href="#" class="d-flex gap-3 mb-2">
+                                <div class="sb-cover-frame">
+                                    <img src="../assets/plate1.jpg" alt="product">
+                                </div>
+                                <div class="sb-card-tp py-2 pe-2">
+                                    <h4 class="sb-card-title">Insalata</h4>
+                                    <div class="price">6.50 €</div>
+                                </div>
+                            </a>
+                            <div class="quantity_input mb-4">
+                                <div class="plus">-</div>
+                                <input type="number" value="1" min="1" max="10">
+                                <div class="minus">+</div>
+                            </div>
+                            <!-- //order -->
+
+                            <!-- order -->
+                            <a href="#" class="d-flex gap-3 mb-2">
+                                <div class="sb-cover-frame">
+                                    <img src="../assets/hambuerger.jpg" alt="product">
+                                </div>
+                                <div class="sb-card-tp py-2 pe-2">
+                                    <h4 class="sb-card-title">Insalata</h4>
+                                    <div class="price">6.50 €</div>
+                                </div>
+                            </a>
+                            <div class="quantity_input mb-4">
+                                <div class="plus">-</div>
+                                <input type="number" value="1" min="1" max="10">
+                                <div class="minus">+</div>
+                            </div>
+                            <!-- //order -->
+                            <!-- order -->
+                            <a href="#" class="d-flex gap-3 mb-2">
+                                <div class="sb-cover-frame">
+                                    <img src="../assets/plate1.jpg" alt="product">
+                                </div>
+                                <div class="sb-card-tp py-2 pe-2">
+                                    <h4 class="sb-card-title">Insalata</h4>
+                                    <div class="price">6.50 €</div>
+                                </div>
+                            </a>
+                            <div class="quantity_input mb-4">
+                                <div class="plus">-</div>
+                                <input type="number" value="1" min="1" max="10">
+                                <div class="minus">+</div>
+                            </div>
+                            <!-- //order -->
+
+                            <!-- order -->
+                            <a href="#" class="d-flex gap-3 mb-2">
+                                <div class="sb-cover-frame">
+                                    <img src="../assets/hambuerger.jpg" alt="product">
+                                </div>
+                                <div class="sb-card-tp py-2 pe-2">
+                                    <h4 class="sb-card-title">Insalata</h4>
+                                    <div class="price">6.50 €</div>
+                                </div>
+                            </a>
+                            <div class="quantity_input mb-4">
+                                <div class="plus">-</div>
+                                <input type="number" value="1" min="1" max="10">
+                                <div class="minus">+</div>
+                            </div>
+                            <!-- //order -->
+                            <!-- order -->
+                            <a href="#" class="d-flex gap-3 mb-2">
+                                <div class="sb-cover-frame">
+                                    <img src="../assets/plate1.jpg" alt="product">
+                                </div>
+                                <div class="sb-card-tp py-2 pe-2">
+                                    <h4 class="sb-card-title">Insalata</h4>
+                                    <div class="price">6.50 €</div>
+                                </div>
+                            </a>
+                            <div class="quantity_input mb-4">
+                                <div class="plus">-</div>
+                                <input type="number" value="1" min="1" max="10">
+                                <div class="minus">+</div>
+                            </div>
+                            <!-- //order -->
+
+                            <!-- order -->
+                            <a href="#" class="d-flex gap-3 mb-2">
+                                <div class="sb-cover-frame">
+                                    <img src="../assets/hambuerger.jpg" alt="product">
+                                </div>
+                                <div class="sb-card-tp py-2 pe-2">
+                                    <h4 class="sb-card-title">Insalata</h4>
+                                    <div class="price">6.50 €</div>
+                                </div>
+                            </a>
+                            <div class="quantity_input mb-4">
+                                <div class="plus">-</div>
+                                <input type="number" value="1" min="1" max="10">
+                                <div class="minus">+</div>
+                            </div>
+                            <!-- //order -->
+                            
+                        </div>
+
+                            <div class="cart_footer border-top py-3">
+                                <!-- button -->
+                                <a href="#" class="view_order btn rounded-0 text-white me-2">
+                                    <span>Visualizza</span>
+                                </a>
+                                <!-- button end -->
+
+                                <!-- button -->
+                                <a href="#" class="pay btn rounded-0 text-white">
+                                    <span>Pagamento</span>
+                                </a>
+                                <!-- button end -->
+
+                                <h3>Totale</h3>
+                                <h4>34.00 €</h4>
+                            </div>
+                            <!-- //cart_footer -->
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+    
+    </div>
+      <!-- //cart_preview -->
 </template>
 
 <style lang="scss" scoped>
@@ -85,6 +270,12 @@ nav{
             font-size: 1.3rem;
            } 
         &:hover{
+                color: #8ea61d;
+            }
+}
+.cart_icon{
+    color:#a43c28;
+    &:hover{
                 color: #8ea61d;
             }
 }
@@ -135,5 +326,84 @@ nav{
 .navbar-collapse {
 flex-grow: unset;
 }
+    .cart_preview {
+        transition: 0.5;
+    background-color: #0000004f;
+    width: 100%;
+    height: 100vh;
+    position: fixed;
+    top: 60px;
+    right: 0;
+    bottom: 0;
+    z-index: 1000;
+.cart_right{
+    position: fixed;
+    top: 60px;
+    right: 0;
+    bottom: 0;
+    z-index: 1000;
+    background-color: #FFFFFF;
+    display: flex;
+    flex-direction: column;
+    outline: 0;
+    transition: transform .3s ease-in-out;
+
+    .square{
+        background-color: #ffbd59;
+    }
+    img{
+        width: 80px;
+        aspect-ratio: 1/1;
+        object-fit: cover;
+    }
+    .title_frame{
+        color: #8ea61d;
+    }
+    .cart_content{
+        height: 40%;
+         a{
+            background-color:#F6EDDA;
+            text-decoration: none;
+            color:#a43c28;
+            font-size: 1.1rem;
+
+        &:hover{
+                color: #8ea61d;
+            }
+        }
+        .quantity_input{
+            display: flex;
+            align-items: center;
+            .plus, .minus{
+               
+                background-color: #F5C332;
+                width: 30px;
+                height: 30px;
+                text-align: center;
+                line-height: 30px;
+                border: none;
+                transition: 0.3s ease-in-out;
+            }
+        }
+    }
+   .cart_footer{
+
+        .view_order{
+            background-color: #ffbd59;
+            &:hover{
+                background-color: #8ea61d;
+            }
+        }
+        .pay{
+            background-color:#a43c28;
+            &:hover{
+                background-color: #8ea61d;
+            }
+        }
+
+   }
+}
+}
+
 
 </style>
