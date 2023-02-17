5ddaa026906840dc3a7a60629daabb7dff6a7a38
Danielateresa <dany.t_87@hotmail.it>
2023-02-17 17:53:23 +0100

    Edit restaurants cards

--- src/components/AppCategory.vue
+++ src/components/AppCategory.vue
@@ -102,26 +102,28 @@ export default {
 
   <section id="restaurants">
 
-    <div class="search container p-5">
+    <div class="search container-fluid shadow-sm p-5">
 
-      <div class="restaurant_type fs-6 text-uppercase">
+      <div class="container restaurant_type fs-6">
 
-        <div class="row mb-1 justify-content-center">
+        <div class="row">
 
           <h1 class="lh-sm text-center">I nostri ristoranti</h1>
 
-          <p class="text-center fst-italic mb-3">Seleziona la tipologia che preferisci</p>
+          <h3 class="pt-md-4 text-center">Seleziona la tipologia che preferisci</h3>
 
-          <div v-for="type in store.types" class="col-2 mb-3 checkbox">
+          <div v-for="type in store.types" class="col-2 checkbox text-uppercase mt-5">
+
+          
 
             <input type="checkbox" name="types" v-model="store.selectedTypes" :value="type.name" :id="type.id"
-              class="me-3" />
+              class="me-2" />
 
-            <label class="checkbox-text" for="types">{{ type.name }}</label>
+            <label class="checkbox-text fw-bold" for="types">{{ type.name }}</label>
 
           </div>
 
-          <button class="btn btn_danger col-4 my-3" @click="store.callApi()">Avvia ricerca</button>
+          <button class="btn btn_danger col-4 mt-5 m-auto" @click="store.callApi()">Avvia ricerca</button>
 
           <p v-if="store.failed === true" class="text-center text-danger text-lowercase fst-italic ">Seleziona almeno
             una
@@ -133,37 +135,39 @@ export default {
 
     </div>
 
-    <hr class="mb-4">
 
-    <div class="container" v-if="!store.loading && store.restaurants">
+    <div class="container py-4" v-if="!store.loading && store.restaurants">
       <div class="row">
 
         <div v-for="restaurant in store.restaurants" class="col-6 p-3 d-flex align-content-center">
 
-          <div class="row p-5 ms_card">
+          
+          <div class="card rounded-4 border-0 shadow">
+              <div class="row gy-0 p-4">
+              <div class="col-4 d-flex align-content-center flex-wrap">
+                <img :src="store.getImagePath(restaurant.restaurant_image)" alt="" class="img-fluid p-3">
 
-            <div class="col-4 d-flex align-content-center flex-wrap">
-
-              <img :src="store.getImagePath(restaurant.restaurant_image)" alt="" class="img-fluid p-3">
-
-              <router-link class="btn btn_secondary col-12"
-                :to="{ name: 'single-restaurant', params: { id: restaurant.id } }" aria-current="page">Dettaglio<span
+                <router-link class="btn btn_secondary col-12"
+                  :to="{ name: 'single-restaurant', params: { id: restaurant.id } }" aria-current="page">Dettaglio<span
                   class="visually-hidden">(current)</span></router-link>
-
-            </div>
-
-            <div class="col-8 px-3 d-flex align-content-center flex-wrap">
-
-              <div>
-
-                <h4 class="m-0">{{ restaurant.name }}</h4>
-
-                <p class="m-0"><strong>Contatti: </strong> {{ restaurant.phone }}</p>
-
-                <p class="m-0"><strong>Indirizzo: </strong> {{ restaurant.address }} </p>
 
               </div>
 
+                <div class="col-8 px-3 d-flex align-content-center flex-wrap">
+
+                  <div class=" text-muted">
+
+                    <h4 class="m-0 green_text text-uppercase fw-bold mb-2">{{ restaurant.name }}</h4>
+                    <span class="fw-bold pe-2"><i class="fa-solid fa-utensils"></i> Categoria: </span>
+                    <span class="pe-3" v-for="type in restaurant.types"><i class="fa-solid fa-hashtag"></i> {{ type.name }} </span>  
+                      
+                      <p class="m-0"><strong><i class="fa-solid fa-phone"></i> Contatti: </strong> {{ restaurant.phone }}</p>
+
+                    <p class="m-0"><strong><i class="fa-solid fa-location-dot"></i> Indirizzo: </strong> {{ restaurant.address }} </p>
+
+                  </div>
+
+                </div>
             </div>
 
           </div>
@@ -209,13 +213,8 @@ export default {
 </template>
 
 <style lang="scss" scoped>
-#special {
-  background-color: #F6EDDA;
-}
 
 #restaurants {
-  background-color: #F6EDDA;
-
   .btn_danger {
     background-color: #A43C28;
     color: #ffffff;
@@ -233,11 +232,6 @@ export default {
     opacity: 1;
     width: 90%;
     margin: auto;
-  }
-
-  .ms_card {
-    background-color: white;
-    border-radius: 5px;
   }
 }
 
@@ -319,4 +313,5 @@ export default {
   font-weight: 600;
   color: #A43C28;
 }
+
 </style>
