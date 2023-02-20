650c8d473dbccecc95faee86bc80a85c7f89ecb2
SimoneLollini <lollinisimone@gmail.com>
2023-02-20 15:26:00 +0100

    change pagination color

--- src/components/AppCategory.vue
+++ src/components/AppCategory.vue
@@ -112,27 +112,29 @@ export default {
 
           <h3 class="pt-md-4 text-center mb-5">Seleziona la tipologia che preferisci</h3>
           <div class="col-12 col-sm-10 m-auto">
-                <div class="card border-0 shadow rounded-4 py-5">
-                  <div class="row w-100 g-0 ps-xl-5">
-                    <div v-for="type in store.types" class="col-12 col-sm-6 checkbox text-uppercase rounded-pill g-1 g-sm-3 m-auto ps-md-5">
+            <div class="card border-0 shadow rounded-4 py-5">
+              <div class="row w-100 g-0 ps-xl-5">
+                <div v-for="type in store.types"
+                  class="col-12 col-sm-6 checkbox text-uppercase rounded-pill g-1 g-sm-3 m-auto ps-md-5">
 
-                  
-                      <div class="w-100 ms-auto ps-5">
-                        <input type="checkbox" name="types" v-model="store.selectedTypes" :value="type.name" :id="type.id"
-                          class="me-0 me-sm-2 ms-lg-5" />
 
-                        <label class="checkbox-text text-muted fs-4" for="types">{{ type.name }}</label>
-                      </div>
+                  <div class="w-100 ms-auto ps-5">
+                    <input type="checkbox" name="types" v-model="store.selectedTypes" :value="type.name" :id="type.id"
+                      class="me-0 me-sm-2 ms-lg-5" />
+
+                    <label class="checkbox-text text-muted fs-4" for="types">{{ type.name }}</label>
                   </div>
-                  </div>
-                  
-                  <button class="btn btn_danger rounded-pill shadow-sm col-8 col-sm-4 mt-5 m-auto" @click="store.callApi()">Avvia ricerca</button>
-                
-                
-                    <p v-if="store.failed === true" class="text-center text-muted text-lowercase mt-2">Seleziona almeno
-                      una
-                      categoria</p>
                 </div>
+              </div>
+
+              <button class="btn btn_danger rounded-pill shadow-sm col-8 col-sm-4 mt-5 m-auto"
+                @click="store.callApi()">Avvia ricerca</button>
+
+
+              <p v-if="store.failed === true" class="text-center text-muted text-lowercase mt-2">Seleziona almeno
+                una
+                categoria</p>
+            </div>
           </div>
 
         </div>
@@ -147,34 +149,37 @@ export default {
 
         <div v-for="restaurant in store.restaurants" class="col-12 col-md-6 p-3 d-flex align-content-center">
 
-          
+
           <div class="card rounded-4 border-0 shadow">
-              <div class="row gy-0 p-4 p-sm-3 p-md-5">
+            <div class="row gy-0 p-4 p-sm-3 p-md-5">
               <div class="col-12 col-sm-4 d-flex align-content-center flex-wrap ">
 
                 <img :src="store.getImagePath(restaurant.restaurant_image)" alt="" class="img-fluid p-3">
 
                 <router-link class="btn btn_secondary rounded-pill col-12 d-none d-sm-block"
                   :to="{ name: 'single-restaurant', params: { id: restaurant.id } }" aria-current="page">Dettaglio<span
-                  class="visually-hidden">(current)</span></router-link>
+                    class="visually-hidden">(current)</span></router-link>
 
               </div>
 
-                <div class="col-12 col-sm-8 px-5 px-sm-2 d-flex flex-column align-content-center flex-wrap text-muted">
+              <div class="col-12 col-sm-8 px-5 px-sm-2 d-flex flex-column align-content-center flex-wrap text-muted">
 
-                    <h3 class=" green_text text-uppercase fw-bold mb-2 mt-4 mt-md-2 mt-xl-3 mt-xxl-4">{{ restaurant.name }}</h3>
-                    <span class="fw-bold pe-2"><i class="fa-solid fa-utensils"></i> Categoria: </span>
-                    <span class="pe-3" v-for="type in restaurant.types"><i class="fa-solid fa-hashtag"></i> {{ type.name }} </span>  
-                      
-                      <p class="m-0"><strong><i class="fa-solid fa-phone"></i> Contatti: </strong> {{ restaurant.phone }}</p>
+                <h3 class=" green_text text-uppercase fw-bold mb-2 mt-4 mt-md-2 mt-xl-3 mt-xxl-4">{{ restaurant.name }}
+                </h3>
+                <span class="fw-bold pe-2"><i class="fa-solid fa-utensils"></i> Categoria: </span>
+                <span class="pe-3" v-for="type in restaurant.types"><i class="fa-solid fa-hashtag"></i> {{ type.name }}
+                </span>
 
-                    <p class="m-0"><strong><i class="fa-solid fa-location-dot"></i> Indirizzo: </strong> {{ restaurant.address }} </p>
+                <p class="m-0"><strong><i class="fa-solid fa-phone"></i> Contatti: </strong> {{ restaurant.phone }}</p>
 
-                    <router-link class="btn btn_secondary rounded-pill col-12 dblock d-sm-none mt-3 mt-sm-0"
+                <p class="m-0"><strong><i class="fa-solid fa-location-dot"></i> Indirizzo: </strong> {{ restaurant.address
+                }} </p>
+
+                <router-link class="btn btn_secondary rounded-pill col-12 dblock d-sm-none mt-3 mt-sm-0"
                   :to="{ name: 'single-restaurant', params: { id: restaurant.id } }" aria-current="page">Dettaglio<span
-                  class="visually-hidden">(current)</span></router-link>
+                    class="visually-hidden">(current)</span></router-link>
 
-                </div>
+              </div>
             </div>
 
           </div>
@@ -196,7 +201,7 @@ export default {
 
           <li class="page-item" v-if="store.results.next_page_url" @click="nextPage(store.results.next_page_url)">
             <a class="page-link" aria-label="Next">
-              <span aria-hidden="true">&raquo;</span>
+              <span class="pagination_arrow" aria-hidden="true">&raquo;</span>
             </a>
           </li>
         </ul>
@@ -220,6 +225,20 @@ export default {
 
 <style lang="scss" scoped>
 @use '../styles/general.scss' as *;
+
+.page-link.active,
+.active {
+  .page-link {
+    background-color: #f7af42;
+    border-color: #f7af42;
+  }
+}
+
+a {
+  span {
+    color: #f7af42;
+  }
+}
 
 
 #special {
@@ -321,34 +340,38 @@ export default {
 }
 
 input[type=checkbox] {
-	-moz-appearance:none;
-	-webkit-appearance:none;
-	-o-appearance:none;
-	outline: none;
-	content: none;	
+  -moz-appearance: none;
+  -webkit-appearance: none;
+  -o-appearance: none;
+  outline: none;
+  content: none;
 }
+
 input[type=checkbox]:before {
-	font-family: "FontAwesome";
-    content: "\f00c";
-    font-size: 15px;
-    color: transparent !important;
-    background: #fef2e0;
-    display: block;
-    width: 15px;
-    height: 15px;
-    margin-right: 7px;
-    margin-top: 5px;;
+  font-family: "FontAwesome";
+  content: "\f00c";
+  font-size: 15px;
+  color: transparent !important;
+  background: #fef2e0;
+  display: block;
+  width: 15px;
+  height: 15px;
+  margin-right: 7px;
+  margin-top: 5px;
+  ;
 }
 
 input[type=checkbox]:checked:before {
 
-	color: #8ea61d !important;
+  color: #8ea61d !important;
 }
+
 .checkbox-text {
   font-weight: 600;
   color: $db_danger;
 }
-.bg_yellow{
+
+.bg_yellow {
   background: #ffbd59;
 }
 </style>
