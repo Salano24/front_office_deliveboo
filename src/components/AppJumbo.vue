6e7e2fd225a3f288f7be051f87f540178856c284
Danielateresa <dany.t_87@hotmail.it>
2023-02-21 09:25:23 +0100

    Fix jumbo

--- src/components/AppJumbo.vue
+++ src/components/AppJumbo.vue
@@ -5,16 +5,35 @@ export default {
 </script>
 
 <template>
-<div class="jumbo w-100"></div>
+<div class="jumbo">
+  <div class="container-xxl py-5">
+    <div class="row g-0 h-75 m-auto">
+        <div class="col-12 col-lg-9 text-center d-flex flex-column justify-content-center m-auto">
+            <img class="img-fluid" src="../assets/vuoi_ordinare.png" alt="">
+            <h2 class="fw-bold pt-4">Consegna veloce nella tua città</h2>
+            <img class="delivery_icon mx-auto" src="../assets/delivery_icon.png" alt="">
+        </div>
+    </div>
+  </div>
+</div>
 </template>
 
 <style lang="scss" scoped>
 .jumbo{
-    background-image: url(../assets/jumbo.png);
-    width: 100vw;
-    aspect-ratio: 16 / 8;
-    
+    background-image: url(../assets/background.png);
+  
     background-repeat: no-repeat;
     background-size: cover;
+    img .img-fluid{
+        object-fit:cover;
+        max-width: 500px;
+    }
+    h2{
+        color:#a43c28;
+    }
+    .delivery_icon{
+        width: 50px;
+    }
 }
+
 </style>
